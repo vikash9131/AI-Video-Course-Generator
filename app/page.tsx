d631089beac2c04b2d29290cd235b206226374fc
vikash9131 <vikashkumar3165676@gmail.com>
2026-07-03 13:49:11 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return(
+    <div>
+      <center>
+      <h2>hello world</h2>
+      <Button>click me</Button>
+      </center>
+    </div>
+  );
+
+}
